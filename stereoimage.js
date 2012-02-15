83d6e5fe130e358cfa2bd114345aa24692a13a55
Matthew Wilcoxson <unknown>
2012-02-15 18:24:36 +0000

    Anaglyph test

--- stereoimage.js
+++ stereoimage.js
@@ -2,12 +2,12 @@ function StereoImage( image ) {
     var imgBoth = image;
     
     var displayType = ['actual_size','size_to_canvas','size_to_canvas_with_aspect'];
-    var drawType = ["bothHorizontal","bothVertical","left","right","flick","stereoscopic"];
+    var drawType = ["bothHorizontal","bothVertical","left","right","flick","stereoscopic","anaglyph"];
     
     /* settings */
     var flickSpeed = 150;
     var _display = displayType[0];
-    var  _drawing = drawType[5];
+    var  _drawing = drawType[3];
     
     var c = document.getElementById("myCanvas");
     var ctx = c.getContext("2d");
@@ -31,6 +31,8 @@ function StereoImage( image ) {
                 return that.flick();
             case( "stereoscopic" ):
                 return that.stereoscopic();
+            case( "anaglyph" ):
+                return that.anaglyph();
             default:
                 alert("Missing _drawing");
         }
@@ -73,15 +75,20 @@ function StereoImage( image ) {
         }
     };
     
-    function drawLeft() {
+    function drawLeft(context) {
         var size = sizes( _display );
-        ctx.drawImage(imgBoth,0,0,imgBoth.width/2,imgBoth.height,0,0,size.w,size.h);
-    }
+        if( context === undefined )
+            context = ctx;
+            
+        context.drawImage(imgBoth,0,0,imgBoth.width/2,imgBoth.height,0,0,size.w,size.h);
+    };
     
-    function drawRight() {
+    function drawRight(context) {
         var size = sizes( _display );
-        ctx.drawImage(imgBoth,imgBoth.width/2,0,imgBoth.width/2,imgBoth.height,0,0,size.w,size.h);
-    }
+        if( context === undefined )
+            context = ctx;
+        context.drawImage(imgBoth,imgBoth.width/2,0,imgBoth.width/2,imgBoth.height,0,0,size.w,size.h);
+    };
     
     this.setSizing = function ( size ) {
         if( size >= 0 && size < displayType.length )
@@ -146,7 +153,7 @@ function StereoImage( image ) {
     this.stereoscopic = function() {
         _drawing = "stereoscopic";
         
-        // TODO 
+        // TODO: reduce image size for easier combination, may also crop image and possibly scroll...
         
         var size = sizes( _display );
         
@@ -158,7 +165,7 @@ function StereoImage( image ) {
         
         clear();
         
-        // Overright height for this setting so that the aspect is always right.
+        // Overright height for this setting so that the aspect is always correct.
         var scale = (size.w*1.0) / imgBoth.width;
         size.h = imgBoth.height * scale;
         
@@ -171,6 +178,38 @@ function StereoImage( image ) {
         
         ctx.drawImage(imgBoth,0,0,imgBoth.width, imgBoth.height,0,imagePosY,size.w, size.h );
     }
+    
+    this.anaglyph = function() {
+        _drawing = "anaglyph";
+        
+        // TODO: reduce image size for easier combination, may also crop image and possibly scroll...
+        
+        var size = sizes( _display );
+        
+        var leftBuffer = document.createElement('canvas');
+        var rightBuffer = document.createElement('canvas');
+        
+        leftBuffer.width =  rightBuffer.width = size.w;//actualImageWidth;
+        leftBuffer.height = rightBuffer.height = size.h;//actualImageHeight;
+        
+        var ctxLeft = leftBuffer.getContext("2d");
+        var ctxRight = rightBuffer.getContext("2d");
+        
+        drawLeft( ctxLeft );
+        drawRight( ctxRight );
+        
+        //http://axon.physik.uni-bremen.de/research/stereo/color_anaglyph/
+        red = ctxLeft.getImageData( 0,0, size.w, size.h);
+        greenblue = ctxLeft.getImageData( 0,0, size.w, size.h);
+        
+        for( var h=0;h<greenblue.height;h++) {
+            for( var w=0; w < greenblue.width;w++ ) {
+                greenblue.data[(h*greenblue.width + w ) * 4 + 0] = 0;
+            }
+        }
+        
+        ctx.putImageData( greenblue,0,0);
+    };
     
     callDrawing( this, _drawing );
 };
